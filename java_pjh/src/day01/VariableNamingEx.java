d19f7719a53492034b498e89d5703f546ca2f0cb
pjh1202 <qkrwlsgus147@naver.com>
2023-12-08 17:14:59 +0900

    자바 1일차 : 변수명 작성 규칙 예제

---
+++ java_pjh/src/day01/VariableNamingEx.java
@@ -0,0 +1,35 @@
+package day01;
+
+public class VariableNamingEx {
+
+	public static void main(String[] args) {
+		//변수명 작성 규칙과 관례
+		//대소문자 구분함
+		int num;
+		int NUM;
+		int Num;
+		int nuM;
+		//예약어 사용 불가능
+		/*int int 
+		int public
+		int static*/
+		
+		//숫자로 '시작' 불가능
+		int num2;
+		//int 2num;
+		
+		//특수문자는 _ 와 $만 가능
+		int num1_1;
+		//int num!;
+		int num1$1;
+		//int num1 1;
+		
+		//중복선언 불가능
+		int num100;
+		//int num;
+		
+		String test;
+		
+	}
+
+}
