7662f6d1a22c5de4c9c42738fc76eb063d85f61e
pjh1202 <qkrwlsgus147@naver.com>
2023-12-14 08:25:56 +0900

    자바 별 마름모

--- java_pjh/src/day04/StarEx5.java
+++ java_pjh/src/day04/StarEx5.java
@@ -3,17 +3,23 @@ package day04;
 public class StarEx5 {
 
 	public static void main(String[] args) {
-		/*
-		 * * 1 3 5 7 9 7 5 3 1
-		 */
 		
-		int row = 5;
-		for (int i=1; i<=5; i++) {
-			for(int j=1; j<=row - i; j++) {
-				System.out.print(" ");
+		for(int i=1; i<=9; i++) {
+			if(i<=5) {
+				for(int j=1; j<=5-i; j++) {
+					System.out.print(" ");
+				}
+				for(int j=1; j<=2*i-1; j++) {
+					System.out.print("*");
+				}
 			}
-			for(int j=1; j<=2*i-1; j++) {
-				System.out.print("*");
+			else { 
+				for(int j=1; j<=i-5; j++) {
+					System.out.print(" ");
+				}
+				for(int j=1; j<=19-(2*i); j++) {
+					System.out.print("*");
+				}
 			}
 			System.out.println();
 		}
