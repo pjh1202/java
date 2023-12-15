7ee9822279938fa742521a6eb7dc7cfcfc1b55e6
pjh1202 <qkrwlsgus147@naver.com>
2023-12-15 12:38:25 +0900

    자바 6일차 : 배열 확장 예제

--- java_pjh/src/day06/ArrayExpand1.java
+++ java_pjh/src/day06/ArrayExpand1.java
@@ -6,7 +6,7 @@ public class ArrayExpand1 {
 		// 배열이 꽉 찼을 때 배열을 확장하는 예제
 		int arr[] = new int[5];
 		int count = 10;
-		//arr는 5자리인데 10번 반복하므로 오류남
+		
 		for(int i=0; i<count; i++) {
 			if((i+1) > arr.length) {
 				int tmp[] = new int[arr.length + 5];
@@ -15,8 +15,8 @@ public class ArrayExpand1 {
 			}
 			arr[i] = i+1;
 		}
-		for(int tmp:arr) {
-			System.out.print(tmp);
+		for(int i:arr) {
+			System.out.print(i+" ");
 		}
 	}
 }
