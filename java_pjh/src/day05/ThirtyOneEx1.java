47a02e0e6dc42e3e8ca2fb628e2f94a3a90760ae
pjh1202 <qkrwlsgus147@naver.com>
2023-12-15 09:00:05 +0900

    자바 5일차 : 31게임

--- java_pjh/src/day05/ThirtyOneEx1.java
+++ java_pjh/src/day05/ThirtyOneEx1.java
@@ -14,49 +14,53 @@ public class ThirtyOneEx1 {
 		int com=0, user, num=0, max=3, min=1;
 		
 		while(true) {
-			int arr[] = new int[6];
 			System.out.println();
 			System.out.print("부르고 싶은 수의 갯수를 쓰시오 : ");
 			user = scanner.nextInt();
 			com = 0;
+			System.out.print("유저 : ");
 			for(int i=0; i<user; i++) {
 				num++;
 				System.out.print(num + " ");
-			}
+				}
 			
 			if(num>=31) {
 				System.out.println("컴퓨터의 승리");
 				break;
-			}
+				}
 			while(com==0) {
 				com = (int)(Math.random()*((max-min+1)+min));
-			}
+				}
 			switch(com) {
 			case 1: {
 				num++;
-				System.out.print(num+" ");
+				System.out.print("\n컴퓨터 : "+num+" ");
 				break;
-			}
+				}
 			case 2: {
+				System.out.print("\n컴퓨터 : ");
 				for(int i=0; i<com; i++) {
 						num++;
 						System.out.print(num+" ");
-				}
+						}
+				System.out.println();
 				break;
-			}
+				}
 			case 3: {
+				System.out.print("\n컴퓨터 : ");
 				for(int i=0; i<com; i++) {
 						num++;
 						System.out.print(num+" ");
-				}
+						}
+				System.out.println();
 				break;
-			}		
+				}		
 			}
 			
 			if(num>=31) {
 				System.out.println("유저의 승리");
 				break;
-			}
+				}
 			
 			System.out.println();
 			System.out.println("com : " + com);
